6ed77eb68f920bb7a6fbea2e7f1244f6488d552f
kkh <kkho9654@naver.com>
2024-07-29 10:58:51 +0900

    :sparkles: Feat: 환경변수 정리

---
+++ Backend/src/main/java/com/ssafy/meshroom/backend/global/common/properties/OpenViduProperties.java
@@ -0,0 +1,16 @@
+package com.ssafy.meshroom.backend.global.common.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "openvidu")
+public class OpenViduProperties {
+    private String url;
+    private String secret;
+}
