2ee784779f6103f9b680f23cde714469764410d4
kkh <kkho9654@naver.com>
2024-08-13 20:35:01 +0900

    feat: ball ui

--- Frontend/lolmushroom/src/components/contents/BallGrow/BallContent.vue
+++ Frontend/lolmushroom/src/components/contents/BallGrow/BallContent.vue
@@ -1,20 +1,66 @@
 <script setup>
 import { computed } from 'vue'
-import { useBallStore } from '@/stores/ballStore'
+import BlueBallComponent from '@/components/contents/BallGrow/BlueBallComponent.vue'
+import GreenBallComponent from '@/components/contents/BallGrow/GreenBallComponent.vue'
+// import { useBallStore } from '@/stores/ballStore'
 
 const props = defineProps({
-  ball: Object
+  ball: Object,
+  isMain: Boolean,
+  isGreen: Boolean
 })
-const ballStore = useBallStore()
-const ballImage = computed(() => ballStore.getBallImage(props.ball.sessionId))
+// const ballStore = useBallStore()
+// const ballImage = computed(() => ballStore.getBallImage(props.ball.sessionId))
 const ballSize = computed(() => props.ball.size)
-console.log(ballImage.value)
+const _isMain = computed(() => props.isMain)
+const groupName = ref("그룹이름!!!");
+
+// 우리 그룹이면 초록색으로 랜더링 해야댐
+// TODO::vif로 isGreen이 true이면 초록색 랜더링 해야함
+const _isGreen = computed(() => props.isGreen)
+// console.log(ballImage.value)
 </script>
 
 <template>
-  <div>
-    <v-img :src="ballImage" :style="{ width: ballSize, height: ballSize }"> </v-img>
+  <div class="main-cotainer">
+    <div class="ball-name">{{ groupName }}</div>
+    <div class="ball-status">{{ ballSize }}</div>
+    <div class="ball-container">
+      <!-- v-if="_isGreen" <GreenBallComponent class="main-ball" :health="ballSize" :isMain="_isMain"/>-->
+      <!-- ball 싸이즈와 메인인지 여부를 주면 볼크기를 랜더링 해준다. -->
+      <BlueBallComponent class="main-ball" :health="ballSize" :isMain="_isMain"/>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-cotainer{
+  position: relative; /* b 태그 위치를 고정하기 위한 상대적 포지션 */
+  width: 600px;
+  height: 400px;
+}
+.ball-name{
+  position: absolute;
+    top: 10px;
+    left: 10px;
+    padding: 5px; /* 필요에 따라 패딩 조절 */
+    background-color: rgba(255, 255, 255, 0.8);
+}
+.ball-status{
+  position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); /* 상태 표시를 가운데에 정확히 위치시키기 위해 사용 */
+    pointer-events: none;
+    z-index: 1;
+}
+.ball-container{
+  
+  border: 1px solid black;
+  border-radius: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- Frontend/lolmushroom/src/components/contents/BallGrow/BallGroup.vue
+++ Frontend/lolmushroom/src/components/contents/BallGrow/BallGroup.vue
@@ -6,11 +6,29 @@ import BallContent from './BallContent.vue'
 const ballStore = useBallStore()
 const totalBalls = computed(() => ballStore.getTotalBalls)
 console.log(totalBalls.value)
+
+
+// 기존 배열을 두개씩 끊어서 저장하는 rows배열
+const rows = computed(() => {
+  return props.totalBalls.reduce((acc, item, index) => {
+    if (index % 2 === 0) acc.push([]);
+    acc[acc.length - 1].push(item);
+    return acc;
+  }, []);
+});
+
 </script>
 
 <template>
   <div class="ball-group">
-    <BallContent v-for="ball in totalBalls" :key="ball.sessionId" :ball="ball" />
+    <!-- 원래 코드 -->
+    <!-- <BallContent v-for="ball in totalBalls" :key="ball.sessionId" :ball="ball" :isGreen="true"/> -->
+
+    <!-- 두줄씩 출력하기 위한 rows배열(2*n) -->
+    <div v-for="(row, rowIndex) in rows" :key="rowIndex" class="row">
+      <!-- TODO: isGreen ==> ball.sessionId == userSubSessionId 를 반환하면 됨, 즉 내 세션이면 초록색으로 렌더링 한다는 뜻 -->
+      <BallContent v-for="ball in row" :key="ball.sessionId" :ball="ball" :isGreen="true" />
+    </div>
   </div>
 </template>
 
