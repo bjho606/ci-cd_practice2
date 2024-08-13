34bedd87636b3bb432472a65e828f6ce13547b5f
이규석 <didie8520@gmail.com>
2024-08-13 19:00:39 +0900

    :bug: socket, store 버그 수정

---
+++ Backend/src/main/java/com/ssafy/meshroom/backend/domain/topic/game/initialquiz/dto/IniQuizAnswerRequestSignal.java
@@ -0,0 +1,15 @@
+package com.ssafy.meshroom.backend.domain.topic.game.initialquiz.dto;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class IniQuizAnswerRequestSignal {
+    private String ovToken;
+    private String userName;
+    private String guessWord;
+}
