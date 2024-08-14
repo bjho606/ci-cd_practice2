57a6f4abb88de6876d431bb8c1ba5dae0173c21c
kkh <kkho9654@naver.com>
2024-08-14 11:11:38 +0900

    !HOTFIX : add param

--- Backend/src/main/java/com/ssafy/meshroom/backend/domain/contents/dao/ContentsOrderRepository.java
+++ Backend/src/main/java/com/ssafy/meshroom/backend/domain/contents/dao/ContentsOrderRepository.java
@@ -4,14 +4,15 @@ import com.ssafy.meshroom.backend.domain.contents.domain.ContentsOrder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ContentsOrderRepository extends MongoRepository<ContentsOrder, String> {
-    List<ContentsOrder> findAllBySessionId(String sessionSid);
+    List<ContentsOrder> findAllBySessionId(@Param("sessionSid") String sessionSid);
 
     @Query("{ '_id' : ?0 }")
     @Update(" {'$set': { 'isDone' : ?1 }} ")
-    void updateById(String _id, Boolean isDone);
+    void updateById(@Param("_id") String _id,@Param("isDone") Boolean isDone);
 
 }
